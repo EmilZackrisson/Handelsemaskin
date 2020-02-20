4b94b2b27ea51290b506782365bf0bc3f341660b
Emil Zackrisson <emil.zackrisson@gmail.com>
2020-02-20 17:34:17 +0100

    Create rubberbandShooter.ino

---
+++ rubberbandShooter/rubberbandShooter.ino
@@ -0,0 +1,26 @@
+const int pResistor = A0;
+const int limit = 40;
+int value;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+
+  digitalWrite(9, HIGH);
+}
+
+void loop() {
+  value = analogRead(pResistor);
+  Serial.println(value);
+  delay(100);
+
+  if(value => limit){
+    digitalWrite(9, LOW); //Disengage brake
+    analogWrite(3, 255);  //Spin motor at full speed
+    delay(500);           //Wait 0.5 sec
+    digitalWrite(9, HIGH); //Engage brake
+    analogWrite(3, 0);  //Set motor speed to 0
+  }
+}
