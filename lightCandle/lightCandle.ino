0317b42077d9efcb94beacef8aecea478af43f63
Emil Zackrisson <emil.zackrisson@gmail.com>
2020-02-24 16:16:49 +0100

    Update lightCandle.ino

--- lightCandle/lightCandle.ino
+++ lightCandle/lightCandle.ino
@@ -5,13 +5,15 @@ Servo myservo;
 int trigPin = 9;
 int echoPin = 10;
 long duration, cm, inches;
+
+int pos = 180;
  
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   myservo.attach(8);
-  myservo.write(90);
+  myservo.write(0);
 }
  
 void loop() {
@@ -29,11 +31,17 @@ void loop() {
   
   delay(250);
 
-  if(cm <= 10){
+  if(cm >= 10){
     lightCandle();
   }
 }
 
 void lightCandle(){
-  myservo.write(0);
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+  delay(9999999999999);
 }
